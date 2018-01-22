a250d13b66873bc26a97a1fb11ec2ac13c9efa5b
Carina Motta <carinamotta@herbarium.ccber.ucsb.edu>
2018-01-22 10:56:05 -0800

    adding iris analysis

--- iris.R
+++ iris.R
@@ -7,5 +7,10 @@ ggplot(iris, aes(Petal.Length, Petal.Width, color = Species)) + geom_point()
 
 #now try changing for sepal.length and sepal.width
 
+ggplot(iris, aes(Sepal.Length, Sepal.Width, color = species)) + geom_point()
 
+#UPGMA hiearchical clustering
 
+set.seed(20)
+irisCluster <- kmeans(iris[, 3:4], 3, nstart = 20)
+irisCluster
